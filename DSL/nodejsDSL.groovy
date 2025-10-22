2944aaa78f53d2462850a9aec822f96a49e604cf
jblanco020889-ctrl <jblanco020889@gmail.com>
2025-10-22 14:22:27 +0200

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/jblanco020889-ctrl/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('jblanco020889-ctrl')
+            node / gitConfigEmail('jblanco020889@gmail.com')
         }
     }
     triggers {
